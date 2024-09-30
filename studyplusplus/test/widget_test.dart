5505e7eed6f4c78ab72ee5e6b2c56771e925457d
Lucas Gualtierigit config --global user.email lucasgualtieri.dev@gmail.com <lucasgualtieri.dev@gmail.com>
2024-09-30 14:09:49 -0300

    Update: changing dir

--- studyplusplus/test/widget_test.dart
+++ studyplusplus/test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../../../CC-PUC/LDDM/lib/main.dart';
+import 'package:studyplusplus/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    // await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(TaskApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
